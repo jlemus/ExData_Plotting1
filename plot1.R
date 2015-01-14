a6b09604bb43655b4cb7097bb4d799adfe31e839
Juan M. Lemus <lemus.juanma@gmail.com>
2015-01-14 14:43:22 -0500

    First commit to add the plot1.R file

---
+++ plot1.R
@@ -0,0 +1,22 @@
+Date1 <- strptime("2006/12/16 17:24:00", "%Y/%m/%d %H:%M:%S")
+Date2 <- strptime("2007/02/01 00:00:00", "%Y/%m/%d %H:%M:%S")
+
+delta_hours <- abs(as.numeric(difftime(Date1, Date2, units = "mins")))
+
+con <- file("/Users/TaxiMagic/Courses/exploratory data analysis/course project 1/data/household_power_consumption.txt")
+
+open(con)
+
+data <- read.table(con, sep = ";", header = TRUE, nrows = 24*60*2, skip = delta_hours)
+
+head(data)
+tail(data)
+
+close(con)
+
+con2 <- file("/Users/TaxiMagic/Courses/exploratory data analysis/course project 1/data/household_power_consumption.txt")
+open(con2)
+
+names <- read.table(con2, sep = ";", header = TRUE, nrows = 1)
+names <- colnames(names)
+colnames(data) <- names
